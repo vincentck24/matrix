77a2bb8694a7d42a6c3c0a818f241bc26fd7a480
vincentck24 <119962705+vincentck24@users.noreply.github.com>
2026-06-17 09:56:16 +0200

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'matrix-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
